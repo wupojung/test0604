2ac83ea6674e57b3229bb3cbf1b4bd88085148db
wupojung <wupojung@gmail.com>
2024-06-04 14:49:17 +0800

    add pic

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class Test : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // Start is called before the firstframe update
     void Start()
     {
-        
+         
     }
 
     // Update is called once per frame
